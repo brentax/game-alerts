c525010cc4925a4f6adaf5d8cfabb6160fb53d5b
Brent Axthelm <brent@bandpass.fm>
2015-02-13 02:27:44 -0500

    Add model tests for game, and validation to game model.

--- spec/models/game_spec.rb
+++ spec/models/game_spec.rb
@@ -1,5 +1,34 @@
 require 'rails_helper'
 
 RSpec.describe Game, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:game) { build(:game) }
+
+  subject { game }
+
+  it { should respond_to(:name) }
+  it { should respond_to(:steam_id) }
+  it { should respond_to(:gog_id) }
+  it { should respond_to(:humblestore_id) }
+
+  it { should be_valid }
+
+  describe "without a name" do
+    before { game.name = " " }
+    it { should_not be_valid }
+  end
+
+  describe "without a steam_id" do
+    before { game.steam_id = " " }
+    it { should be_valid }
+  end
+
+  describe "without a gog_id" do
+    before { game.gog_id = " " }
+    it { should be_valid }
+  end
+
+  describe "without a humblestore_id" do
+    before { game.humblestore_id = " " }
+    it { should be_valid }
+  end
 end
