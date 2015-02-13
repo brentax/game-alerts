17f6a88137956e5dbbc13c10a975d55c4fcc0ee2
Brent Axthelm <brent@bandpass.fm>
2015-02-13 03:54:52 -0500

    Run `rake db:migrate`

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150213023101) do
+ActiveRecord::Schema.define(version: 20150213084546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150213023101) do
     t.string   "humblestore_id"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email"
+    t.string   "edit_code"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
