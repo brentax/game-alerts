6b466a4a25272d87b6b1c8397b1c7ad41abd072b
Brent Axthelm <brent@bandpass.fm>
2015-02-13 03:54:32 -0500

    Move users to 'api/v1'.

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,3 +1,6 @@
+module Api
+module V1
+
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
 
@@ -21,7 +24,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created, location: @user
+      render json: @user, status: :created, location: api_v1_user_url(@user)
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -56,4 +59,7 @@ class UsersController < ApplicationController
     def user_params
       params.require(:user).permit(:email, :edit_code)
     end
+end
+
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :users, except: [:new, :edit]
   namespace :api, defaults: { format: 'json' } do
     namespace :v1 do
       resources :games, except: [:new, :edit]
+      resources :users, except: [:new, :edit]
     end
   end
 end
